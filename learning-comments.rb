8bc54098bfd3dd1878ae05c0befd6853591edb66
Rick Glascock <rixong@gmail.com>
2020-01-22 18:33:36 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,24 @@
+# # Perform a constant expression evaluation
+# 3
+# #Assign constant 3 to bareword variable triangle_sides
+# 'triangle_sides'
+
+# #From three dog night song
+# puts "Joy to the world"
+# puts "All the boys and girls"
+# puts "Joy to you and me"
+
+# i=2
+# while (i<=257) do
+#   break if i == 100 + 28
+#   print i
+#   print'.'*10
+#   print "\n"
+#   i *= 2
+# end
+
+3.times do
+  puts "Here we go"
+end
+  
+
---
+++ play.rb
@@ -0,0 +1,12 @@
+# i = 1
+# 10.times do
+#   puts i
+#   i +=1
+# end
+
+
+i=0
+until i == 10
+puts i
+i += 1
+end
